7707a4841c04ebb0f5243f546120a48847630ffe
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2020-05-13 15:58:01 +0200

    added one table in DB

--- Backend/dataDB.js
+++ Backend/dataDB.js
@@ -23,6 +23,22 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 db.run(insert, ["Nicklas","1234","Nicklas@test.com"])
                 db.run(insert, ["PG","1234","PG@test.com"])
                 db.run(insert, ["Christoffer","1234","toffe@test.com"])
+
+            }
+        })
+        db.run('CREATE TABLE Scoreboard (' +
+            'id INTEGER PRIMARY KEY,' +
+            'userId INTEGER,score TEXT,' +
+            'datetime TEXT )',(err) => {
+            if (err) {
+                // Table already created
+            }else {
+                // Table just created, creating some rows
+                var insert2 = 'INSERT INTO Scoreboard (userId,score,datetime) VALUES(?,?,?)'
+                db.run(insert2, ["3", "100", "2020-01-01"])
+                db.run(insert2, ["1", "200", "2020-01-01"])
+                db.run(insert2, ["2", "300", "2020-01-01"])
+                db.run(insert2, ["4", "400", "2020-01-01"])
             }
         })
     }
