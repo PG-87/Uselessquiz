fddf387b7bdb36e15c88491dcc5e2bf25ffcde7b
Patrik <pg.patrik@gmail.com>
2020-05-18 12:15:05 +0200

    fixed questions and cleaned up in app.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -24,17 +24,12 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        /*margin-top: 60px;*/
     }
 
     .logo {
         width: 100px;
         height: 100px;
         margin-top: 0px;
-    }
-
-    hr {
-        /*width: 800px;*/
     }
 
     a {
