ab6e792d43aacaead9869021bad955ffe73f1aba
Patrik <pg.patrik@gmail.com>
2020-05-25 12:17:33 +0200

    added a nice timer

--- Frontend/src/components/Timer.vue
+++ Frontend/src/components/Timer.vue
@@ -1,67 +1,175 @@
 <template>
     <div class="base-timer">
-        <svg
-                class="base-timer__svg"
-                viewBox="0 0 100 100"
-                xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g class="base-timer__circle">
-                <circle
-                        class="base-timer__path-elapsed"
-                        cx="50"
-                        cy="50"
-                        r="10"
-                />
+                <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                <path
+                        :stroke-dasharray="circleDasharray"
+                        class="base-timer__path-remaining"
+                        :class="remainingPathColor"
+                        d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+                ></path>
             </g>
         </svg>
-        <span class="base-timer__label">
-      <!-- Remaining time label -->
-    </span>
+        <span class="base-timer__label">{{ formattedTimeLeft }}</span>
     </div>
 </template>
 
 <script>
-    // export default {
-    //     name: "Timer"
-    // }
-    export default {
-        name: "Timer",
-            computed: {
-        formattedTimeLeft() {
-            const timeLeft = this.timeLeft
-            // The largest round integer less than or equal
-            // to the result of time divided being by 60.
-            const minutes = Math.floor(timeLeft / 60)
-            // Seconds are the remainder of the time divided
-            // by 60 (modulus operator)
-            let seconds = timeLeft % 60
-            // If the value of seconds is less than 10,
-            // then display seconds with a leading zero
-            if (seconds < 10) {
-                seconds = `0${seconds}`
-            }
-            // The output in MM:SS format
-            return `${minutes}:${seconds}`
+    const FULL_DASH_ARRAY = 283;
+    const WARNING_THRESHOLD = 10;
+    const ALERT_THRESHOLD = 5;
+
+    const COLOR_CODES = {
+        info: {
+            color: "green"
+        },
+        warning: {
+            color: "orange",
+            threshold: WARNING_THRESHOLD
+        },
+        alert: {
+            color: "red",
+            threshold: ALERT_THRESHOLD
         }
-    }
-    }
+    };
+
+    var timeLimit = 30;
+
+    export default {
+        data() {
+            return {
+                timePassed: 0,
+                timerInterval: null,
+                points: 0
+            };
+        },
+
+        computed: {
+            circleDasharray() {
+                return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
+            },
+
+            formattedTimeLeft() {
+                const timeLeft = this.timeLeft;
+                const minutes = Math.floor(timeLeft / 60);
+                let seconds = timeLeft % 60;
+
+                if (seconds < 10) {
+                    seconds = `0${seconds}`;
+                }
+
+                return `${minutes}:${seconds}`;
+            },
+
+            timeLeft() {
+                return timeLimit - this.timePassed;
+            },
+
+            timeFraction() {
+                const rawTimeFraction = this.timeLeft / timeLimit;
+                return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
+            },
+
+            remainingPathColor() {
+                const {alert, warning, info} = COLOR_CODES;
+
+                if (this.timeLeft <= alert.threshold) {
+                    return alert.color;
+                } else if (this.timeLeft <= warning.threshold) {
+                    return warning.color;
+                } else {
+                    return info.color;
+                }
+            }
+        },
+
+        watch: {
+            timeLeft(newValue) {
+                if (newValue === this.points) {
+                    this.onTimesUp();
+                }
+            }
+        },
+
+        // mounted() {
+        //     this.startTimer();
+        // },
+
+        methods: {
+            onTimesUp() {
+                clearInterval(this.timerInterval);
+            },
+            stopTimer() {
+              this.points = (timeLimit - this.timePassed) - 0.5
+            },
+            startTimer() {
+                this.points = 0;
+                this.timerInterval = setInterval(() => (this.timePassed += 0.5), 500);
+            },
+            resetTimer(){
+                this.timePassed = 0;
+            }
+        }
+    };
 </script>
 
 <style scoped lang="scss">
-    /* Sets the containers height and width */
     .base-timer {
         position: relative;
         width: 300px;
         height: 300px;
-        /* Removes SVG styling that would hide the time label */
+
+        &__svg {
+            transform: scaleX(-1);
+        }
+
         &__circle {
             fill: none;
             stroke: none;
         }
-        /* The SVG path that displays the timer's progress */
+
         &__path-elapsed {
             stroke-width: 7px;
-            stroke:grey;
+            stroke: grey;
+        }
+
+        &__path-remaining {
+            stroke-width: 7px;
+            stroke-linecap: round;
+            transform: rotate(90deg);
+            transform-origin: center;
+            transition: 1s linear all;
+            fill-rule: nonzero;
+            stroke: currentColor;
+
+            &.green {
+                color: rgb(65, 184, 131);
+            }
+
+            &.orange {
+                color: orange;
+            }
+
+            &.red {
+                color: red;
+            }
+        }
+
+        &__label {
+            position: absolute;
+            width: 300px;
+            height: 300px;
+            top: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 48px;
         }
     }
 </style>
