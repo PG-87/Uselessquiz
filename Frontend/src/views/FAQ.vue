da485eca7d1c2593368c69e764dceddf3c43fa84
Tugba A <karaca.tgb@gmail.com>
2020-06-02 11:36:35 +0200

    added icons to FAQ page

--- Frontend/src/views/FAQ.vue
+++ Frontend/src/views/FAQ.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <ul id="questions" v-for="question in questions" v-bind:key="question.id">
-            <li @click="showAnswer(question.id)" style="font-weight: bolder">{{question.Question}} + </li>
-            <li v-show="question.isSelected">{{question.Answer}}</li>
-
+            <li v-on:click="showAnswer(question.id)" style="font-weight:bold; color:#2c3e50">{{question.Question}} <font-awesome-icon :icon="[ 'fas', 'angle-double-down' ]"
+                                                                                                                                         v-if="!question.isSelected"/> </li>
+            <li v-on:click="showAnswer(question.id)" v-show="question.isSelected">{{question.Answer}} <i class="fas fa-angle-double-up"></i></li>
         </ul>
     </div>
 </template>
@@ -25,13 +25,13 @@
                     {
                         id: 2,
                         Question: "Vad ska jag göra om jag glömmer min lösenord?",
-                        Answer: "Glöm inte! We have no solution for this",
+                        Answer: "Glöm inte! We have no solution for this.",
                         isSelected: false,
                     },
                     {
                         id: 3,
                         Question: "Hur kan jag vinna hela tiden?",
-                        Answer: "Right click on the page, go to inspect button, you will see the file for questions and right answers",
+                        Answer: "Right click on the page, go to inspect button, you will see the file for questions and right answers.",
                         isSelected: false,
                     }
                 ]
@@ -61,6 +61,24 @@
 
     #questions{
         list-style-type: none;
+    }
+    hr {
+        width: 100%;
+        margin-bottom: 27px;
+        margin-top: 0;
+        padding: 0;
+    }
+
+    #questions {
+        margin-top: 0;
+        padding-left: 0;
+        padding-bottom: 10px;
+    }
+
+    #questions li {
+        padding-bottom: 0;
+        font-size: 20px;
+        cursor: pointer;
     }
 
 </style>
